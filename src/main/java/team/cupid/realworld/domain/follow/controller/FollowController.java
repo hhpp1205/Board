81ffb58664eb8fe7cab2d1d9fab339c12dc83252
hsm <hhpp000222@gamil.com>
2023-04-06 16:36:31 +0900

    팔로잉 조회 팔로우 조회 컨트롤러 API 추가

--- src/main/java/team/cupid/realworld/domain/follow/controller/FollowController.java
+++ src/main/java/team/cupid/realworld/domain/follow/controller/FollowController.java
@@ -5,9 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import team.cupid.realworld.domain.follow.dto.FollowResponse;
 import team.cupid.realworld.domain.follow.dto.SimpleFollowResponse;
 import team.cupid.realworld.domain.follow.service.FollowService;
 import team.cupid.realworld.global.security.principal.CustomUserDetails;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,16 +21,28 @@ public class FollowController {
 
     @PostMapping("/follow/{followerId}")
     public ResponseEntity<SimpleFollowResponse> following(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-                                                       @PathVariable Long followerId) {
+                                                          @PathVariable Long followerId) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new SimpleFollowResponse(followService.following(customUserDetails.getId(), followerId)));
     }
 
     @DeleteMapping("/follow/{followerId}")
     public ResponseEntity<Void> unFollowing(@AuthenticationPrincipal CustomUserDetails customUserDetails,
-                            @PathVariable Long followerId) {
+                                            @PathVariable Long followerId) {
         followService.unFollowing(customUserDetails.getId(), followerId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/follow/following")
+    public ResponseEntity<List<FollowResponse>> getFollowing(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return followService.getFollowing(customUserDetails.getId());
+    }
+
+    @GetMapping("/follow/follower")
+    public ResponseEntity<List<FollowResponse>> getFollower(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return followService.getFollower(customUserDetails.getId());
     }
 
 }
--- src/main/java/team/cupid/realworld/domain/follow/service/FollowService.java
+++ src/main/java/team/cupid/realworld/domain/follow/service/FollowService.java
@@ -1,7 +1,11 @@
 package team.cupid.realworld.domain.follow.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import team.cupid.realworld.domain.follow.dto.FollowResponse;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +19,15 @@ public class FollowService {
 
     public void unFollowing(Long followingId, Long followerId) {
         // TODO: 2023/04/06  
+    }
+
+    public ResponseEntity<List<FollowResponse>> getFollowing(Long memberId) {
+        // TODO: 2023/04/06  
+        return null;
+    }
+
+    public ResponseEntity<List<FollowResponse>> getFollower(Long memberId) {
+        // TODO: 2023/04/06  
+        return null;
     }
 }
