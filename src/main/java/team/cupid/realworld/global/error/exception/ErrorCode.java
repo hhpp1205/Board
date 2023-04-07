803ba1a590ce975c7ab08643e906d14417fe0bdb
hsm <hhpp000222@gamil.com>
2023-04-08 00:12:45 +0900

    ErrorCode에 Member ErrorCode 추가

--- src/main/java/team/cupid/realworld/global/error/exception/ErrorCode.java
+++ src/main/java/team/cupid/realworld/global/error/exception/ErrorCode.java
@@ -7,7 +7,12 @@ public enum ErrorCode {
     METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
     INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
     INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
-    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied");
+    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
+
+    // Member
+    MEMBER_NOT_FOUND(400, "M001", "존재하지 않는 회원입니다."),
+    DUPLICATE_EMAIL(400, "M002", "존재하는 이메일입니다."),
+    DUPLICATE_NICKNAME(400, "M003", "존재하는 닉네임입니다.");
 
     private final String code;
     private final String message;
